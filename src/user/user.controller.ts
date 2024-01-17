4d6a324365fc42df2903ec34cc7fbb55ca5616f8
pedrobm1 <pedrobm1rblx@gmail.com>
2024-01-17 02:35:17 -0300

    first commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+import { User } from '@prisma/client';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+    @Get('me')
+    getMe(@GetUser() user: User) {
+        return user;
+    }
+
+    @Patch()
+    editUser() {
+        
+    }
+}
